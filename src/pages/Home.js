58d78f13d4512849ae7befec76c0fa465020b751
muhamedessamz <mohamedessamzakariaa@gmail.com>
2026-01-24 21:22:41 +0200

    feat: Implement initial frontend structure including the home page, authentication styling, menu data, and API client setup.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,14 @@ import WhyChooseUs from '../components/home/WhyChooseUs';
 import Testimonials from '../components/home/Testimonials';
 import ContactCTA from '../components/home/ContactCTA';
 
+import PizzaBuilderTeaser from '../components/home/PizzaBuilderTeaser';
+
 const Home = () => {
   return (
     <div className="home-page">
       <HeroSection />
       <FeaturedDishes />
+      <PizzaBuilderTeaser />
       <WhyChooseUs />
       <Testimonials />
       <ContactCTA />
